2a5d846ffaf3ffed46d320f8c5c87373785c2cc1
kekszharcos <b.enedek670@gmail.com>
2023-11-05 22:58:22 +0100

    valtozasok serveren belüli felépítésben, indexen a járatok listázása,userprofban lassan admin funkcio

--- server.js
+++ server.js
@@ -11,29 +11,36 @@ const initializePassport = require("./passport-config")
 const flash = require("express-flash")
 const session = require("express-session")
 const path = require("path");
-const favicon = require('serve-favicon');
 const method_override = require("method-override")
 const pool = require("./database.js")
+const validator = require("email-validator");
 
 
 let users = []
-let felhasznaloksql = 'SELECT * FROM felhasznalo'
-pool.execute(felhasznaloksql,function (err,res){
-    if (err) throw err
-    res.forEach(kek=>{
-        users.push(kek)
-
+function updateUsers () {
+    let login_sql = 'SELECT * FROM felhasznalo'
+    pool.execute(login_sql,function (err,res){
+        if (err) console.log(err)
+        res.forEach(kek=>{
+            users.push(kek)
+        })
+        console.log(users)
     })
-    console.log(users)
-})
+}
+updateUsers()
 
 initializePassport(
     passport,
     email=> users.find(felhasznalo => felhasznalo.email === email),
     id => users.find(felhasznalo => felhasznalo.id === id)
 )
-app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use("/css",express.static(path.join(__dirname, 'public/css')))
+app.use("/js",express.static(path.join(__dirname, 'public/js')))
+app.use("/sources",express.static(path.join(__dirname, 'public/sources')))
+
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(flash())
 app.use(session({
@@ -54,8 +61,9 @@ app.post("/login", checkNotAuthenticated,passport.authenticate("local",{
 //Register post config
 app.post("/register",checkNotAuthenticated ,async (req,res,done)=>{
     try {
-        const hashed_password = await bcrypt.hash(req.body.password, 10)
-        if (!users.find(felhasznalo => felhasznalo.email === req.body.email)){
+        let uzi = []
+        if (!users.find(felhasznalo => felhasznalo.email === req.body.email) && req.body.password === req.body.password_again && validator.validate(req.body.email) && req.body.username !== ""){
+            const hashed_password = await bcrypt.hash(req.body.password, 10)
             users.push({
                 id: Date.now().toString(),
                 nev: req.body.username,
@@ -64,13 +72,21 @@ app.post("/register",checkNotAuthenticated ,async (req,res,done)=>{
                 szerep: "user"
             })
             let adando = users.find(felhasznalo => felhasznalo.email === req.body.email)
-            let felhasznaloksql = 'INSERT INTO felhasznalo (id, nev, email, jelszo, szerep) VALUES (' +
+            let felhasznalo_hozzaadas_sql = 'INSERT INTO felhasznalo (id, nev, email, jelszo, szerep) VALUES (' +
                 "'" + adando.id + "', '" + adando.nev + "', '" + adando.email + "', '" + adando.jelszo + "', '" + adando.szerep + "');";
-            pool.execute(felhasznaloksql)
+            pool.execute(felhasznalo_hozzaadas_sql)
             res.redirect("/login")
+
+        }else if (users.find(felhasznalo => felhasznalo.email === req.body.email) && req.body.password === req.body.password_again && validator.validate(req.body.email) && req.body.username !== ""){
+            uzi.push("This email is occupied!")
+            res.render("register.ejs",{uzi:uzi})
+        }else if (req.body.password !== req.body.password_again){
+            uzi.push("Passwords must match!")
+            res.render("register.ejs",{uzi:uzi})
         }else {
-           let uzi = "Az email már foglalt."
+            res.redirect("/register")
         }
+        //
 
         console.log(users)
 
@@ -79,9 +95,49 @@ app.post("/register",checkNotAuthenticated ,async (req,res,done)=>{
         res.redirect("/register")
     }
 })
+
+let listas = [];
+pool.execute('SELECT allomas.nev, datum, idopont, tipus FROM jarat INNER JOIN allomas ON allomas.azonosito = jarat.celallomasazon',function (err,res){
+    if (err) console.log(err)
+    res.forEach(kek=>{
+        listas.push(kek)
+    })
+});
+app.post("/admin",  (req, res) => {
+    res.render("userprof.ejs", { nev: req.user.nev + " you are in admin mode", szerep: req.user.szerep, lista: listas });
+});
+
 //Útvonal
 app.get("/", (req, res)=>{
-    res.render("index.ejs")
+    /*function myFunction() {
+        const pool = require('./database.js')
+        let myInput, filter, ul, li, a, i, txtValue;
+        let lista = []
+        myInput = document.getElementById('myInput')
+        filter = "'%"+myInput.value()+"%'"
+        console.log(filter)
+        pool.execute('SELECT allomas.nev, datum, idopont, tipus FROM jarat INNER JOIN allomas ON allomas.azonosito = jarat.celallomasazon WHERE nev LIKE '+filter,function (err,res){
+            if (err) console.log(err)
+            res.forEach(kek=>{
+                lista.push(kek)
+            })
+        });
+        ul = document.getElementById("myUl");
+        li = ul.getElementsByTagName('li');
+
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+
+    }*/
+    res.render("index.ejs",{lista:listas})
 })
 app.get("/login", checkNotAuthenticated,(req,res)=>{
     res.render("login.ejs")
@@ -89,15 +145,18 @@ app.get("/login", checkNotAuthenticated,(req,res)=>{
 app.get("/register", checkNotAuthenticated, (req,res)=>{
     const isEmailOccupied = users.some(felhasznalo => felhasznalo.email === req.body.email);
     if (isEmailOccupied){
-        res.render("register.ejs", { uzi: "Az email már foglalt." });
+        res.render("register.ejs");
     }
-    res.render("register.ejs" )
+    res.render("register.ejs");
+
 })
 app.get("/userprof", checkAuthenticated ,(req,res)=>{
-    res.render("userprof.ejs",{nev: req.user.nev})
+    res.render("userprof.ejs",{nev: req.user.nev, szerep: req.user.szerep, lista: ""})
 })
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(favicon(path.join(__dirname,'public','sources','favico.ico')))
+app.get("/admin", checkAuthenticated,checkAdminPermission ,(req,res)=>{
+    res.render("userprof.ejs",{nev: req.user.nev, szerep: req.user.szerep, lista: listas})
+})
+
 //utvonalak vege
 
 app.delete("/logout", (req,res)=>{
@@ -122,5 +181,11 @@ function checkNotAuthenticated(req,res,next){
     next()
 }
 
+function checkAdminPermission(req,res,next){
+    if(req !== "admin"){
+        return res.redirect("/userprof")
+    }
+    next()
+}
 
 app.listen(3000)
